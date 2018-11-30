cf1b9d805b13099ff7e0cc6f3b71cebcec8bb6a5
luoxiangnan <luoxn28@163.com>
2018-11-30 13:06:18 +0800

    Create FindXxxFromGitSource.cmd

---
+++ FindXxxFromGitSource.cmd
@@ -0,0 +1,119 @@
+### 从git仓库源码中查找是否包含某个字符串的工程
+
+1. 引入hutool依赖
+```xml
+<dependency>
+    <groupId>cn.hutool</groupId>
+    <artifactId>hutool-all</artifactId>
+    <version>4.1.19</version>
+</dependency>
+```
+
+2.运行代码
+```java
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.RuntimeUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 从git仓库源码中查找是否包含符合条件的服务
+ */
+public class FindXxxFromGitSource {
+
+    /**
+     * linux window 系统文件路径分隔符
+     */
+    private static char systemSeparator = FileUtil.isWindows() ? '\\' : '/';
+
+    private static String gitPathPrefix = "git@192.168.1.xxx:xxx/";
+
+    /**
+     * 文件后缀，非空
+     */
+    static String fileSuffix = ".java";
+
+    /**
+     * 待查找字符串
+     */
+    static String findStr = "xxx";
+
+
+    public static void main2(String[] args) {
+        System.out.println(RuntimeUtil.exec("cmd cd d:\n"));
+    }
+
+    public static void main(String[] args) {
+        String gitsPath = System.getProperty("user.dir") + systemSeparator + "gits.txt";
+        if (!FileUtil.exist(gitsPath)) {
+            System.out.println("输入文件不存在: " + gitsPath);
+            return;
+        }
+
+        String gitDir = System.getProperty("user.dir") + systemSeparator + "temp";
+        if (FileUtil.exist(gitDir)) {
+            System.out.println("临时目录已存在: " + gitDir);
+        }
+
+        List<String> result = new ArrayList<>();
+        FileUtil.readUtf8Lines(gitsPath).stream().filter(StrUtil::isNotBlank)
+                .distinct().forEach(gitName -> {
+            // git clone
+            String savePath = gitDir + systemSeparator + gitName + systemSeparator;
+            if (!FileUtil.exist(savePath)) {
+                System.out.print(RuntimeUtil.execForStr(
+                        "git clone " + gitPathPrefix + gitName + ".git " + savePath));
+            } else {
+                System.out.print(gitName + ": " + JSONUtil.toJsonStr(RuntimeUtil.execForStr("git -C " + savePath + " pull")));
+            }
+
+            Boolean flag = false;
+            String fileName = "";
+            List<String> fileList = getAllFilePath(Arrays.asList(savePath), fileSuffix);
+            for (String file : fileList) {
+                if (FileUtil.readUtf8String(file).contains(findStr)) {
+                    flag = true;
+                    fileName = StrUtil.subAfter(file, systemSeparator, true);
+                    break;
+                }
+            }
+
+            // 包含某个字符串
+            if (flag) {
+                result.add(gitName + " : " + fileName);
+            }
+        });
+
+        System.out.println("包含符合查找条件的服务有:");
+        result.forEach(System.out::println);
+    }
+
+    /**
+     * 递归获取文件夹下所有符合后缀名称的文件
+     */
+    static List<String> getAllFilePath(List<String> pathList, String suffix) {
+        List<String> result = new ArrayList<>();
+        List<String> other = new ArrayList<>();
+        pathList.forEach(path -> Arrays.stream(FileUtil.ls(path)).forEach(file -> {
+            if (file.isFile()) {
+                if (StrUtil.isNotBlank(suffix) && file.getAbsolutePath().endsWith(suffix)) {
+                    result.add(file.getAbsolutePath());
+                }
+            } else {
+                other.add(file.getAbsolutePath());
+            }
+        }));
+
+        if (CollectionUtil.isNotEmpty(other)) {
+          result.addAll(getAllFilePath(other, suffix));
+        }
+        return result;
+    }
+
+}
+```
